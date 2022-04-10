810160bd91f72878259746a144005a57e38144e6
rafifalaziz <mrafifalaziz26@gmail.com>
2022-04-10 17:27:19 +0700

    delete food, get food, list food with pagination

--- controller/food.js
+++ controller/food.js
@@ -52,3 +52,93 @@ exports.editFood = async (req, res) => {
         });
     }
 }
+
+exports.deleteFood = async (req, res) => {
+    try {
+        await FOOD.destroy({
+            where: {
+                id_food: req.params.id,
+            }
+        });
+        res.status(200).send({
+            success: true,
+            message: 'Resep berhasil dihapus',
+            code: 200,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Resep gagal dihapus',
+            code: 500,
+            error,
+        });
+    }
+}
+
+exports.getFood = async (req, res) => {
+    try {
+        const food = await FOOD.findOne({
+            where: {
+                id_food: req.params.id,
+            }
+        });
+        if(food){
+            res.status(200).send({
+                success: true,
+                message: 'Resep berhasil didapatkan',
+                code: 200,
+                food,
+            });
+        }
+        res.status(403).send({
+            success: false,
+            message: 'Resep tidak ditemukan',
+            code: 403,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Resep gagal didapatkan',
+            code: 500,
+            error,
+        });
+    }
+}
+
+exports.listFood = async (req, res) => {
+    try {
+        const limit = parseInt(req.params.limit);
+        const page = parseInt(req.params.page);
+        const offset = (page-1)*limit;
+        const foods = await FOOD.findAndCountAll({
+            limit: limit,
+            offset: offset,
+            where: {},
+        });
+        if(foods){
+            res.status(200).send({
+                success: true,
+                message: 'Resep berhasil didapatkan',
+                code: 200,
+                foods,
+                currentPage: page,
+                limit: limit,
+            });
+        }
+        res.status(403).send({
+            success: false,
+            message: 'Resep tidak ditemukan',
+            code: 403,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Resep gagal didapatkan',
+            code: 500,
+            error,
+        });
+    }
+}
--- routes/food.js
+++ routes/food.js
@@ -7,6 +7,8 @@ const routes = express.Router();
 
 routes.post('/add', verifyToken, imageUpload.imageUpload, food.addFood);
 routes.put('/update/:id', verifyToken, imageUpload.imageUpload, food.editFood);
-
+routes.delete('/delete/:id', verifyToken, food.deleteFood);
+routes.get('/get/:id', verifyToken, food.getFood);
+routes.get('/list/:limit&:page', verifyToken, food.listFood);
 
 module.exports = routes;
